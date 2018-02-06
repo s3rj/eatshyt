2f5dee9444ac6cd1ed804a430db4e39f1be9ea8f
Will <wmccrory@gmail.com>
2018-02-06 13:55:02 -0600

    Adding background color rotation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,18 @@
+var searchCollapse = false
+
 $("#collapseSearch").on("click", function () 
 {
-	
+	if (searchCollapse === false)
+	{
+		searchCollapse = true;
+		$("#searchElement").animate({left: '-50%'}, 200);
+		$(".titleSplash").fadeTo("slow", 0);
+		$("#collapseSearch").css({transform: 'rotate(180deg)'});
+	} 
+	else
+	{
+		searchCollapse = false
+		$("#searchElement").animate({left: '0%'}, 200);
+		$("#collapseSearch").css({transform: 'rotate(0deg)'});
+	}
 });
