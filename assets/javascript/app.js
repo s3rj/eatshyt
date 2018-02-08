4112f4308533a97ac21aed606e9cb38ebc472917
Will <wmccrory@gmail.com>
2018-02-07 20:45:13 -0600

    Scope switch on project. Updated html/css. Touchup on site design. Added content display. Added bandsintown API functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,23 @@
 //eatShyt////////////////////////////////////
 
+ ////////////////////////////////////////////
 //variable bank/////////////////////////////
 
 var searchCollapse = false //variable to determine which way to slide search
 var footerCollapse = false //variable to determine status of footer menu
 
+var inputArtist //taking input value from search
+var bitQuery //url query for bands in town API
+var bitResponse //var storing bands in town API response
+
+var artistName //name of artist being searched
+
+var latLongArray = [] //Array holding pertinent information from BIT API call
+
+ ///////////////////////////////////////////
 //function bank////////////////////////////
 
-//search sliding mechanics
+//search sliding mechanics//
 
 function searchSlide ()
 {
@@ -16,16 +26,21 @@ function searchSlide ()
 		searchCollapse = true;
 		$("#searchElement").animate({left: '-93%'}, 200);
 		$(".titleSplash").fadeTo("slow", 0);
-		$("#collapseSearch").css({transform: 'rotate(180deg)'});
+		$("#sideBar").fadeTo("slow", 0);
+		$("#collapseSearch").html("<i class='fas fa-angle-double-left'></i>");
+		setTimeout(function () {
+			$("#contentDisplay").fadeIn(1000);
+		}, 500);
 	} 
 	else
 	{
 		searchCollapse = false
 		$("#searchElement").animate({left: '0%'}, 200);
-		$("#collapseSearch").css({transform: 'rotate(0deg)'});
+		$("#collapseSearch").html("<i class='fas fa-angle-double-right'></i>");
 	}
 };
 
+//footer menu mechanics//
 function footerSlide ()
 {
 	if (footerCollapse === false)
@@ -42,6 +57,49 @@ function footerSlide ()
 	}
 }
 
+//API stuff///////////////////
+
+//Bands in Town API call//
+function bitAPI (artist)
+{
+	bitQuery = "https://rest.bandsintown.com/artists/" + artist + "/events/?app_id=seeShyt";
+	$.ajax({
+		url: bitQuery,
+		method: "GET"
+	}).then(function(response) 
+	{
+		if (response.length < 1)
+		{
+			$("#contentHeadline").empty();
+			$("#contentHeadline").append("<h1>No upcoming shows dude");
+		}
+		else
+		{
+		bitResponse = response;
+		artistName = $("<h1>").text(bitResponse["0"].lineup["0"]);
+		$("#contentHeadline").empty();
+		$("#contentHeadline").append("<h1>" + bitResponse["0"].lineup["0"] + "</h1><h2>" + bitResponse.length + " upcoming shows.");
+		latLongPull()
+		console.log(latLongArray)
+		}
+	})
+}
+
+//Pulling pertinent information out of BIT API call//
+function latLongPull ()
+{
+	for (i = 0; i < bitResponse.length; i++) 
+	{
+		latLongArray.push({
+			"name": bitResponse[i].venue.name,
+			"latitude": bitResponse[i].venue.latitude,
+			"longitude": bitResponse[i].venue.longitude,
+			"date": bitResponse[i].datetime,
+		})
+	}
+}
+
+ ///////////////////////////////////////////
 //site progression/////////////////////////
 
 $("#collapseSearch").on("click", function () 
@@ -53,3 +111,10 @@ $("#footerMenu").on("click", function ()
 {
 	footerSlide();
 });
+
+$("#submit").on("click", function(event) {
+	event.preventDefault();
+	searchSlide();
+	inputArtist = $("#searchBar").val().trim();
+	bitAPI(inputArtist);
+})
